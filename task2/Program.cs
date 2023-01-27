b81771525d239f0e558788cc1dcbede5f1dfd67d
Diamond <fkvfptq1408@gmail.com>
2023-01-27 13:43:31 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int num = new Random().Next();
+int temp = num;
+int reverseNum = 0;
+
+    while(temp > 0)
+        {
+            reverseNum = (reverseNum * 10) + (temp % 10);
+            temp = temp / 10;
+        }
+    
+    if(reverseNum == num)
+        {
+            System.Console.WriteLine($"{num} -> yes");
+        }
+    
+    else
+        {
+            System.Console.WriteLine($"{num} -> no");
+        }       
