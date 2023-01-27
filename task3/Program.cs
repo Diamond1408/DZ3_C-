1d56287295f7c71f4586d6dfabca6c87c6ea8429
Diamond <fkvfptq1408@gmail.com>
2023-01-27 13:55:56 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N
+
+int num = new Random().Next(1, 21);
+System.Console.Write($"{num} -> 1");
+
+    for (int i = 2; i <= num; i++)
+    {
+        double result = Math.Pow(i, 3);
+        System.Console.Write($", {result}");
+    }
