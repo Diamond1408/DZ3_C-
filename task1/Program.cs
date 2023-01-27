b0ee0a8e541a4c5f7ebb25e889962eb7e80470b6
Diamond <fkvfptq1408@gmail.com>
2023-01-27 13:26:37 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Random rnd = new Random();
+int x1 = rnd.Next(-11, 11);
+int y1 = rnd.Next(-11, 11);
+int z1 = rnd.Next(-11, 11);
+int x2 = rnd.Next(-11, 11);
+int y2 = rnd.Next(-11, 11);
+int z2 = rnd.Next(-11, 11);
+
+double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+System.Console.WriteLine($"A({x1}, {y1}, {z1}); B({x2}, {y2}, {z2}) -> {result}");
